f37f93cd99f9d67bbe693a3697b018a2c6dcf46a
BAZINGA <2406357659@qq.com>
2021-01-23 11:53:53 +0800

    update

--- src/main/java/com/example/learn/demo/controller/AccountController.java
+++ src/main/java/com/example/learn/demo/controller/AccountController.java
@@ -1,12 +1,13 @@
 package com.example.learn.demo.controller;
 
 import com.example.learn.demo.modle.Account;
+import com.example.learn.demo.modle.Response;
 import com.example.learn.demo.service.AccountServiceImp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.Map;
+
 
 
 @Slf4j
@@ -17,12 +18,12 @@ public class AccountController {
     private AccountServiceImp accountServiceImp;
 
     @GetMapping("/login")
-    Map<String, Object> Login(@RequestParam String id,@RequestParam String password)
+    Response Login(@RequestParam String id, @RequestParam String password)
     {
         return accountServiceImp.Login(id,password);
     }
     @PostMapping("/register")
-    Map<String, Object> Rigister(@RequestBody Account account)
+    Response Rigister(@RequestBody Account account)
     {
         return accountServiceImp.Rigister(account);
     }
--- src/main/java/com/example/learn/demo/service/AccountService.java
+++ src/main/java/com/example/learn/demo/service/AccountService.java
@@ -1,11 +1,12 @@
 package com.example.learn.demo.service;
 
 import com.example.learn.demo.modle.Account;
+import com.example.learn.demo.modle.Response;
 
 import java.util.Map;
 
 
 public interface AccountService {
-    Map<String, Object> Rigister(Account account);
-    Map<String,Object> Login(String id,String password);
+    Response Rigister(Account account);
+    Response Login(String id,String password);
 }
