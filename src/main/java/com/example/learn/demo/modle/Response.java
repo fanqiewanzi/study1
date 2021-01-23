3446788912865eae8ce002dd65c3c3173b194ed0
BAZINGA <2406357659@qq.com>
2021-01-23 11:58:25 +0800

    update

---
+++ src/main/java/com/example/learn/demo/modle/Response.java
@@ -0,0 +1,11 @@
+package com.example.learn.demo.modle;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Response {
+    private String code;
+    private String message;
+}
